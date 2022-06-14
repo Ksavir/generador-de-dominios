1b02816678366a5ee8adc193f82e17103985cf13
Ksavir <kevinsavir20@gmail.com>
2022-06-14 23:46:14 +0000

    generador de dominio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".net", ".us", ".io"];
+let start = "www.";
+let domains = [];
+
+window.onload = function domain() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < ext.length; d++) {
+          domains.push(start + pronoun[a] + adj[b] + noun[c] + ext[d]);
+        }
+      }
+    }
+  }
+  document.querySelector("p").innerHTML = domains;
 };
